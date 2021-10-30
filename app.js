1693dbd53469efefb63797f19e5af9f22302dd68
Klaus-Mikaelson-007 <iamfunnydeadpool@gmail.com>
2021-10-30 13:13:37 +0530

    Fixed port issue

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const coco = require('@tensorflow-models/coco-ssd')
 
 
 const app = express()
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(__dirname+'/public'));
